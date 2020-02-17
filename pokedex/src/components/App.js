559a6c5c4892fea6ff4b32c8457f35a73e281645
Jenna Goldman <jenna.goldman89@gmail.com>
2020-02-16 19:22:18 -0800

    add Header component to app and link Google Fonts to index.html

--- pokedex/src/components/App.js
+++ pokedex/src/components/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { Router, Route, Switch, Link } from 'react-router-dom';
+import Header from './Header';
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Header />
       </div>
     );
   }
---
+++ pokedex/src/components/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+    render() {
+        return (
+            <header>
+                <h1>Gotta Catch 'Em All!</h1>
+            </header>
+        )
+    }
+}
